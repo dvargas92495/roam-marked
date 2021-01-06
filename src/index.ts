74a077be4e645b4dc767954521012a08820971d8
David Vargas <dvargas92495@gmail.com>
2021-01-06 10:21:46 -0500

    Marked Implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-const run = (): number => {
-  return 0;
+import marked from "marked";
+
+marked.use({
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore should be optional
+  renderer: {
+    text: (text: string) => {
+      let openingTag = false;
+      const highlightRegex = new RegExp("\\^\\^", "g");
+      return text.replace(highlightRegex, (): string => {
+        openingTag = !openingTag;
+        return openingTag ? '<span class="rm-highlight">' : "</span>";
+      });
+    },
+  },
+});
+
+const run = (text: string): string => {
+  return marked(text.replace(new RegExp("__", "g"), "_"));
 };
 
 export default run;
--- tests/intex.test.ts
+++ tests/intex.test.ts
@@ -1,5 +1,15 @@
 import run from "../src";
 
 test("Runs Default", () => {
-  expect(run()).toBe(0);
+  const md = `- A **bolded** text
+- An __italicized__ text
+- A ^^highlighted^^ text
+- A ~~strikethrough~~ text`;
+  expect(run(md)).toBe(`<ul>
+<li>A <strong>bolded</strong> text</li>
+<li>An <em>italicized</em> text</li>
+<li>A <span class="rm-highlight">highlighted</span> text</li>
+<li>A <del>strikethrough</del> text</li>
+</ul>
+`);
 });
