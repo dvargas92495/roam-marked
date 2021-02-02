e2b9c4edaaab2edf904508222ad93d6a4e794527
David Vargas <dvargas92495@gmail.com>
2021-02-02 13:39:20 -0500

    Incorporate bold and italics

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
 import marked from "marked";
 
+const boldRegex = new RegExp("\\*\\*", "g");
+const italicsRegex = new RegExp("__", "g");
+const highlightRegex = new RegExp("\\^\\^", "g");
+
 marked.use({
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore should be optional
   renderer: {
     text: (text: string) => {
       let openingTag = false;
-      const highlightRegex = new RegExp("\\^\\^", "g");
+      let openingBold = false;
+      let openingItalics = false;
       return text.replace(highlightRegex, (): string => {
         openingTag = !openingTag;
         return openingTag ? '<span class="rm-highlight">' : "</span>";
+      }).replace(boldRegex, (): string => {
+        openingBold = !openingBold;
+        return openingBold ? '<span class="rm-bold">' : "</span>";
+      }).replace(italicsRegex, (): string => {
+        openingItalics = !openingItalics;
+        return openingItalics ? '<span class="rm-italics">' : "</span>";
       });
     },
   },
 });
 
 const run = (text: string): string => {
-  return marked(text.replace(new RegExp("__", "g"), "_"));
+  return marked(text);
 };
 
 export default run;
