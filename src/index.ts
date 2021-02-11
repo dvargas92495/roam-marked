84bd5d0c64c2bb1babb9835762e1a1e4bcdb8e37
David Vargas <dvargas92495@gmail.com>
2021-02-11 15:36:26 -0600

    Huge renderer rewrite

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,93 @@
 import marked from "marked";
 
-const boldRegex = new RegExp("\\*\\*", "g");
-const italicsRegex = new RegExp("(?:([^a-zA-Z0-9])_|^_)", "g");
-const highlightRegex = new RegExp("\\^\\^", "g");
+const RENDERED_TODO =
+  '<span><label class="check-container"><input type="checkbox" disabled=""><span class="checkmark"></span></label></span>';
+const RENDERED_DONE =
+  '<span><label class="check-container"><input type="checkbox" checked="" disabled=""><span class="checkmark"></span></label></span>';
+
+const TODO_REGEX = /^{{(?:\[\[)?TODO(?:\]\])?}}/;
+const DONE_REGEX = /^{{(?:\[\[)?DONE(?:\]\])?}}/;
+const BOLD_REGEX = /^\*\*([^*]* )\*\*/;
+const ITALICS_REGEX = /^__([^_]*)__/;
+const HIGHLIGHT_REGEX = /^\^\^([^^]*)\^\^/;
+const INLINE_STOP_REGEX = /({{|\*\*|__|\^\^)/;
+
+const TAG_REGEXES = [TODO_REGEX, DONE_REGEX, HIGHLIGHT_REGEX];
 
 marked.use({
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore should be optional
-  renderer: {
-    text: (text: string) => {
-      let openingTag = false;
-      return text.replace(highlightRegex, (): string => {
-        openingTag = !openingTag;
-        return openingTag ? '<span class="rm-highlight">' : "</span>";
-      });
+  tokenizer: {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore should accept boolean return value
+    tag(src) {
+      for (const r of TAG_REGEXES) {
+        const match = r.exec(src);
+        if (match) {
+          return {
+            type: "html",
+            raw: match[0],
+            text: match[0],
+          };
+        }
+      }
+      return false;
     },
+    emStrong(src: string) {
+      const match = BOLD_REGEX.exec(src);
+      if (match) {
+        return {
+          type: "strong",
+          raw: match[0],
+          text: match[1],
+        };
+      }
+      const emMatch = ITALICS_REGEX.exec(src);
+      if (emMatch) {
+        return {
+          type: "em",
+          raw: emMatch[0],
+          text: emMatch[1],
+        };
+      }
+      return false;
+    },
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore should accept boolean return value
+    inlineText(src) {
+      const match = INLINE_STOP_REGEX.exec(src);
+      if (match) {
+        return {
+          type: "text",
+          raw: src.substring(0, match.index),
+          text: src.substring(0, match.index),
+        };
+      }
+      return false;
+    },
+  },
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore should be optional
+  renderer: {
     strong: (text: string) => `<span class="rm-bold">${text}</span>`,
     em: (text: string) => `<em class="rm-italics">${text}</em>`,
+    html: (text: string) => {
+      if (TODO_REGEX.test(text)) {
+        return RENDERED_TODO;
+      } else if (DONE_REGEX.test(text)) {
+        return RENDERED_DONE;
+      } else if (HIGHLIGHT_REGEX.test(text)) {
+        const match = HIGHLIGHT_REGEX.exec(text);
+        return `<span class="rm-highlight">${match?.[1]}</span>`;
+      } else {
+        return text;
+      }
+    },
   },
 });
 
-const wrap = ({ text, fcn }: { text: string; fcn: (t: string) => string }) => {
-  let openingBold = false;
-  let openingItalics = false;
-  return fcn(text.replace(new RegExp("__", "g"), "_"))
-    .replace(boldRegex, (): string => {
-      openingBold = !openingBold;
-      return openingBold ? '<span class="rm-bold">' : "</span>";
-    })
-    .replace(italicsRegex, (_, preChar): string => {
-      openingItalics = !openingItalics;
-      return openingItalics
-        ? `${preChar}<em class="rm-italics">`
-        : `${preChar}</em>`;
-    });
-};
+export const lexer = marked.lexer;
 
-const run = (text: string): string =>
-  wrap({
-    text,
-    fcn: marked,
-  });
+export const parseInline = marked.parseInline;
 
-export const parseInline = (text: string): string =>
-  wrap({
-    text,
-    fcn: marked.parseInline,
-  });
-
-export default run;
+export default marked;
--- tests/intex.test.ts
+++ tests/intex.test.ts
@@ -1,4 +1,5 @@
-import run from "../src";
+import run, { lexer } from "../src";
+import fs from "fs";
 
 test("Runs Default", () => {
   const md = `- A **bolded** text
@@ -7,7 +8,11 @@ test("Runs Default", () => {
 - A ~~strikethrough~~ text
 - A **bolded ** text
 - An __italicized __ text
-- __italicized __ text`;
+- __italicized __ text
+- A {{[[TODO]]}} This is a todo block
+- A {{[[DONE]]}} This is a done block`;
+
+  fs.writeFileSync("debug.json", JSON.stringify(lexer(md), null, 4));
   expect(run(md)).toBe(`<ul>
 <li>A <span class="rm-bold">bolded</span> text</li>
 <li>An <em class="rm-italics">italicized</em> text</li>
@@ -16,6 +21,8 @@ test("Runs Default", () => {
 <li>A <span class="rm-bold">bolded </span> text</li>
 <li>An <em class="rm-italics">italicized </em> text</li>
 <li><em class="rm-italics">italicized </em> text</li>
+<li>A <span><label class="check-container"><input type="checkbox" disabled=""><span class="checkmark"></span></label></span> This is a todo block</li>
+<li>A <span><label class="check-container"><input type="checkbox" checked="" disabled=""><span class="checkmark"></span></label></span> This is a done block</li>
 </ul>
 `);
 });
